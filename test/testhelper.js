2fbb59b591418053c0b0a2d4567be4916003974c
clx1608@qq.com <hbdennischen@outlook.com>
2019-11-05 17:35:39 +0800

    init

--- test/testhelper.js
+++ test/testhelper.js
@@ -13,7 +13,7 @@ chai.use(chaiAsPromised);
 var sinonStubPromise = require('sinon-stub-promise');
 sinonStubPromise(sinon);
 
-global.dbURI = 'mongodb://127.0.0.1:27017/test';
+global.dbURI = 'mongodb+srv://dhy:9@cluster0-lzapf.mongodb.net/test?retryWrites=true&w=majority';
 global.clearDB = require('@finelets/hyper-rest/db/mongoDb/clearDB')(dbURI);
 
 global.expect = chai.expect;
